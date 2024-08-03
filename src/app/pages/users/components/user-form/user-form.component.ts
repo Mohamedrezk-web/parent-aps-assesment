07760a8089dd820e101e52e018059fd79f903036
Mohamedrezk-web <mohamedrizkmr6@gmail.com>
2024-08-04 01:13:20 +0300

    add file input functionality

--- src/app/pages/users/components/user-form/user-form.component.ts
+++ src/app/pages/users/components/user-form/user-form.component.ts
@@ -1,11 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   EventEmitter,
+  inject,
   input,
   Input,
   OnInit,
   Output,
+  Renderer2,
+  ViewChild,
 } from '@angular/core';
 import { UserViewComponent } from '../../components/user-view/user-view.component';
 import { UserService } from '../../services/user.service';
@@ -17,7 +21,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { UserFormControls, UserFormValue } from '../../models/user-form.model';
-import { NgClass } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -27,7 +31,13 @@ import { RouterLink } from '@angular/router';
   host: {
     class: 'd-flex w-100 h-50',
   },
-  imports: [UserViewComponent, ReactiveFormsModule, NgClass, RouterLink],
+  imports: [
+    UserViewComponent,
+    ReactiveFormsModule,
+    NgClass,
+    RouterLink,
+    NgOptimizedImage,
+  ],
   providers: [UserService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -42,10 +52,18 @@ export class UserFormComponent implements OnInit {
   }
   loading = input(false);
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('imgPreview') imgPreview!: ElementRef;
+
   @Output() onSubmit = new EventEmitter<UserFormValue>();
 
+  renderer: Renderer2 = inject(Renderer2);
   get formControls() {
     return this.userFormGroup.controls;
+  }
+
+  get avatarValueType() {
+    return typeof this.userFormGroup.controls.avatar.value;
   }
   constructor() {}
 
@@ -62,6 +80,42 @@ export class UserFormComponent implements OnInit {
       avatar: new FormControl(''),
       email: new FormControl('', [Validators.required, Validators.email]),
     });
+  }
+
+  triggerFileInputClick() {
+    this.fileInput?.nativeElement.click();
+  }
+
+  clearAvatarValue() {
+    this.userFormGroup.controls.avatar.setValue(null);
+  }
+
+  onFileSelect(event: any) {
+    const file = event.target.files?.[0];
+
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.userFormGroup.get('avatar')?.setValue(e.target.result);
+
+        // Optional: Display a preview of the image
+        this.renderer.setProperty(
+          this.imgPreview.nativeElement,
+          'src',
+          e.target.result
+        );
+      };
+
+      reader.onerror = () => {
+        console.error('Error reading the image file.');
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      // Handle invalid file type
+      this.userFormGroup.get('avatar')?.setErrors({ invalidType: true });
+    }
   }
 
   submitFormValue() {
--- src/app/pages/users/models/user-form.model.ts
+++ src/app/pages/users/models/user-form.model.ts
@@ -5,13 +5,13 @@ export interface UserFormValue {
   first_name: string | null;
   last_name: string | null;
   email: string | null;
-  avatar: string | null;
+  avatar: string | File | null;
 }
 
 export interface UserFormControls {
   first_name: FormControl<string | null>;
   last_name: FormControl<string | null>;
   email: FormControl<string | null>;
-  avatar: FormControl<string | null>;
+  avatar: FormControl<string | File | null>;
   id: FormControl<number | null>;
 }
